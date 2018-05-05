e5c7cf01a6543857cc7e639014c3d683a0a08f99
JasonZhlu <3027089725@qq.com>
2018-05-05 14:21:24 +0800

    测试git

--- src/main/java/com/sumainfo/agency/AgencyApplication.java
+++ src/main/java/com/sumainfo/agency/AgencyApplication.java
@@ -2,6 +2,8 @@ package com.sumainfo.agency;
 
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,11 +14,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 @MapperScan("com.sumainfo.agency.dao")
 public class AgencyApplication extends SpringBootServletInitializer {
-
 	
-	  
-	
-	
+	static Logger log =LoggerFactory.getLogger(AgencyApplication.class);
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         // 注意这里要指向原先用main方法执行的Application启动类
@@ -24,6 +23,7 @@ public class AgencyApplication extends SpringBootServletInitializer {
     }
 
 	public static void main(String[] args) {
+		log.info("汽车项目启动");
 		SpringApplication.run(AgencyApplication.class, args);
 	}
 }
